07da125697a164a4cd8df8166ebc24b4572e0249
Gauravj <gaurav@geniusplaza.com>
2021-02-25 12:04:30 +0530

    helper modified

---
+++ helper.js
@@ -0,0 +1,11 @@
+/**
+ * remove spaces from title and change to lowercase
+ * @param title
+ * @returns {string}
+ */
+export const getKeyForQuestion = title => {
+  return title
+    .toLowerCase()
+    .replace(/\s/g, '')
+    .trim();
+};
